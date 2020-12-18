12b7accb064325b3574cf0fa25520cd34745bf84
JAPL1499 <65917258+JAPL1499@users.noreply.github.com>
2020-12-18 14:01:26 -0500

    Proyecto Finalizado

--- app/src/main/java/com/example/proyecto2/MainActivity4.java
+++ app/src/main/java/com/example/proyecto2/MainActivity4.java
@@ -13,43 +13,26 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity4 extends AppCompatActivity {
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
-    int whip = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (sensor==null)
-            Toast.makeText(MainActivity4.this, "Sensor Unavailabe!", Toast.LENGTH_SHORT).show();
-
-        sensorEventListener = new SensorEventListener() {
+        final Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        SensorEventListener sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                float x = sensorEvent.values[0];
-                float y = sensorEvent.values[1];
-                System.out.println("Valor giro:" + x);
-                if (x<-5 && whip >= 0){
-                    whip++;
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                }else if (x>5 && whip >= 0){
-                    whip++;
+
+                if(sensorEvent.values[0]<proximitySensor.getMaximumRange()){
+
                     getWindow().getDecorView().setBackgroundColor(Color.RED);
-                }
-                else if (y<-5 && whip >= 0){
-                    whip++;
+
+                }else{
                     getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+
                 }
-                else if (y>5 && whip >= 0){
-                    whip++;
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
-                if (whip == 4)
-                    whip =0;
+
             }
 
             @Override
@@ -57,27 +40,11 @@ public class MainActivity4 extends AppCompatActivity {
 
             }
         };
-        start();
-    }
-    private void start(){
-        sensorManager.registerListener(sensorEventListener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    private void stop(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
 
-    @Override
-    protected void onPause() {
-        stop();
-        super.onPause();
-    }
+        sensorManager.registerListener(sensorEventListener,proximitySensor,2 * 1000 *1000);
 
-    @Override
-    protected void onResume() {
-        start();
-        super.onResume();
+        
     }
-
     public void Inicio(View view){
         Intent inicio = new Intent(this, MainActivity.class);
         startActivity(inicio);

299d682615e57ea4ea59ee265cd20b65f1ae61de
Ricardo Cuan <ricardo.cuan@aiesec.net>
2020-12-21 08:10:48 -0500

    cambie el color de pantalla en activiy4

--- app/src/main/java/com/example/proyecto2/MainActivity4.java
+++ app/src/main/java/com/example/proyecto2/MainActivity4.java
@@ -26,7 +26,7 @@ public class MainActivity4 extends AppCompatActivity {
 
                 if(sensorEvent.values[0]<proximitySensor.getMaximumRange()){
 
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
 
                 }else{
                     getWindow().getDecorView().setBackgroundColor(Color.GREEN);
